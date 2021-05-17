3defd06b420d9c0099d90d768d7a10c2df0e1e2f
Khai Do <64216725+KrisKai@users.noreply.github.com>
2021-05-17 15:45:00 +0700

    redesigned UI

--- app/src/main/java/com/example/myprojectyear32/ui/room/LivingroomFragment.java
+++ app/src/main/java/com/example/myprojectyear32/ui/room/LivingroomFragment.java
@@ -4,16 +4,19 @@ package com.example.myprojectyear32.ui.room;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import com.example.myprojectyear32.R;
 import com.example.myprojectyear32.data.notification.Notification;
+import com.example.myprojectyear32.session.SessionManager;
 import com.example.myprojectyear32.ui.bar.HomeFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
@@ -24,18 +27,14 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 
 public class LivingroomFragment extends Fragment {
 
     FloatingActionButton floatingActionButton;
-    CardView lightingCardView;
-    CardView sensorCardView;
-    CardView securityCardView;
-    CardView fanCardView;
     Notification notification;
-    DatabaseReference reference;
-    private Calendar calendar;
-    private SimpleDateFormat dateFormat;
+    DatabaseReference notiReference, statusReference;
+    SwitchCompat mLightBulb, mFan, mSensor, mDoor;
     private String date;
     private long maxID=0;
 
@@ -62,9 +61,15 @@ public class LivingroomFragment extends Fragment {
                     .commit();
         });
 
-        reference = FirebaseDatabase.getInstance().getReference().child("Notification");
+        SessionManager session = new SessionManager(getContext());
+        HashMap<String,String> userDetails = session.getUserDetailFromSession();
+        String userName = userDetails.get(SessionManager.KEY_USERNAME);
 
-        reference.addValueEventListener(new ValueEventListener() {
+        notiReference = FirebaseDatabase.getInstance().getReference().child("User").child(userName).child("Notification");
+
+        statusReference = FirebaseDatabase.getInstance().getReference().child("User").child(userName).child("Status").child("LivingRoom");
+
+        notiReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 maxID = snapshot.getChildrenCount();
@@ -75,28 +80,62 @@ public class LivingroomFragment extends Fragment {
 
             }
         });
-        calendar = Calendar.getInstance();
-        dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
         date = dateFormat.format(calendar.getTime());
-        lightingCardView = (CardView) view.findViewById(R.id.lightingLRCard);
-        lightingCardView.setOnClickListener(v -> {
-            notification = new Notification();
-            notification.setDescription("Bật đèn phòng khách.");
-            notification.setImage(R.mipmap.lighting);
-            notification.setTime(date);
+        mLightBulb = view.findViewById(R.id.lightLRswitch);
+        mLightBulb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    notification = new Notification();
+                    notification.setDescription("Bật đèn phòng khách.");
+                    notification.setImage(R.mipmap.lighting);
+                    notification.setTime(date);
 
-            reference.child(String.valueOf(maxID + 1)).setValue(notification);
-            Toast.makeText(getActivity(), notification.getDescription(), Toast.LENGTH_SHORT).show();
+                    notiReference.child(String.valueOf(maxID + 1)).setValue(notification);
+                    Toast.makeText(getActivity(), notification.getDescription(), Toast.LENGTH_SHORT).show();
+                    statusReference.child("lighting").setValue("True");
+                } else {
+                    statusReference.child("lighting").setValue("False");
+                }
+            }
         });
-        fanCardView = (CardView) view.findViewById(R.id.fanLRCard);
-        fanCardView.setOnClickListener(v -> {
-            notification = new Notification();
-            notification.setDescription("Bật quạt phòng khách.");
-            notification.setImage(R.mipmap.fan);
-            notification.setTime(date);
+        mFan = view.findViewById(R.id.fanLRswitch);
+        mFan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    notification = new Notification();
+                    notification.setDescription("Bật quạt phòng khách.");
+                    notification.setImage(R.mipmap.fan);
+                    notification.setTime(date);
 
-            reference.child(String.valueOf(maxID + 1)).setValue(notification);
-            Toast.makeText(getActivity(), notification.getDescription(), Toast.LENGTH_SHORT).show();
+                    notiReference.child(String.valueOf(maxID + 1)).setValue(notification);
+                    Toast.makeText(getActivity(), notification.getDescription(), Toast.LENGTH_SHORT).show();
+                    statusReference.child("fan").setValue("True");
+                } else {
+                    statusReference.child("fan").setValue("False");
+                }
+            }
+        });
+        mDoor = view.findViewById(R.id.doorLRswitch);
+        mDoor.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    notification = new Notification();
+                    notification.setDescription("Mở cửa phòng khách.");
+                    notification.setImage(R.mipmap.security);
+                    notification.setTime(date);
+
+                    notiReference.child(String.valueOf(maxID + 1)).setValue(notification);
+                    Toast.makeText(getActivity(), notification.getDescription(), Toast.LENGTH_SHORT).show();
+                    statusReference.child("door").setValue("True");
+                } else {
+                    statusReference.child("door").setValue("False");
+                }
+            }
         });
 
         return view;
