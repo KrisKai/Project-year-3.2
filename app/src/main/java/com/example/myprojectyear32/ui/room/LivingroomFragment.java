4db580245cf4df0b9a3276762e872c8e9937fa86
Khai Do <64216725+KrisKai@users.noreply.github.com>
2021-08-15 22:37:23 +0700

    Update LivingroomFragment.java

--- app/src/main/java/com/example/myprojectyear32/ui/room/LivingroomFragment.java
+++ app/src/main/java/com/example/myprojectyear32/ui/room/LivingroomFragment.java
@@ -193,10 +193,8 @@ public class LivingroomFragment extends Fragment {
             },1000);
             MQTTPublisher.Subcriber("living");
             MQTTPublisher.MessageOutput();
-            String message = "null";
-            if(!MQTTPublisher.msg.isEmpty()){
-                message = MQTTPublisher.msg;
-            }
+            String message = MQTTPublisher.msg;
+
             if(message.contains("Temp")){
                 sensorTV.setText(message);
             }
