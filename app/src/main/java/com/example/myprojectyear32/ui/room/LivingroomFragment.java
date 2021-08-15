713eb1e0c84ad7983e5bec8efd649f598b02cb7b
Khai Do <64216725+KrisKai@users.noreply.github.com>
2021-08-15 22:51:20 +0700

    Update LivingroomFragment.java

--- app/src/main/java/com/example/myprojectyear32/ui/room/LivingroomFragment.java
+++ app/src/main/java/com/example/myprojectyear32/ui/room/LivingroomFragment.java
@@ -182,15 +182,18 @@ public class LivingroomFragment extends Fragment {
 
         });
         mSensor.setOnClickListener(v -> {
+            String message = null;
             notification = new Notification();
             notification.setDescription("Cập nhập nhiệt độ phòng.");
             notification.setImage(R.mipmap.sensor);
             notification.setTime(date);
             MQTTPublisher.Connect(getContext(), "192.168.1.200:1883");
             MQTTPublisher.Subcriber("living");
-            MQTTPublisher.MessageOutput();
-            MQTTPublisher.Publisher("sensor");
-            String message = MQTTPublisher.msg;
+            while(message.contains("Temp")){
+                MQTTPublisher.MessageOutput();
+                MQTTPublisher.Publisher("sensor");
+                message = MQTTPublisher.msg;
+            }
             if(message.contains("Temp")){
                 sensorTV.setText(message);
             }
